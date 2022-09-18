d1c8b033cc8ec7c1a6c4df7a97f15a75600971f1
V. K <heyllog@gmail.com>
2022-09-18 23:21:12 +0300

    feat: check versions with interval

--- src/main.js
+++ src/main.js
@@ -3,25 +3,29 @@ import DatabaseService from './services/database/DatabaseService.js'
 import EnvService from './services/EnvService.js'
 import NotificationService from './services/NotificationService.js'
 
-const main = async () => {
+const getVersion = async (appStoreService, database, notificationService) => {
   const { appId } = EnvService.appStoreConfig
-  const appStoreService = new AppStoreService()
-
-  const database = new DatabaseService()
-  const notificationService = new NotificationService(database, EnvService.telegramBotToken)
-
   const appInfo = await appStoreService.getAppInfo({ appId })
-
   const lastWrittenVersion = await database.getVersion(appId)
 
   if (lastWrittenVersion !== appInfo.version) {
     await database.setVersion(appId, appInfo.version)
     const subscribers = await database.getSubscribers(appId)
 
-    console.log(subscribers)
-
     notificationService.notifyAboutNewVersion(subscribers, appInfo)
   }
+}
+
+const main = async () => {
+  const appStoreService = new AppStoreService()
+  const database = new DatabaseService()
+  const notificationService = new NotificationService(database, EnvService.telegramBotToken)
+
+  await getVersion(appStoreService, database, notificationService)
+
+  setInterval(async () => {
+    await getVersion(appStoreService, database, notificationService)
+  }, EnvService.requestConfig.interval)
 }
 
 export default main
