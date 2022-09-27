afae6db8538f8dc7490a964e65347c108e943920
V. K <heyllog@gmail.com>
2022-09-27 21:59:29 +0400

    fix: continue polling after catching error

--- src/services/PollingService.ts
+++ src/services/PollingService.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 import AppStoreService from './StoreServices/AppStoreService'
 import DatabaseService from './database/DatabaseService'
 import BotService from './BotService'
@@ -32,12 +34,14 @@ class PollingService {
       await this.getVersion()
 
       setTimeout(async () => {
-        await this.getVersion()
         await this.startPolling()
       }, EnvService.requestConfig.interval)
     } catch (e) {
-      console.error(e)
-      await this.startPolling()
+      console.log(_.isError(e) ? e.message : e)
+
+      setTimeout(async () => {
+        await this.startPolling()
+      }, EnvService.requestConfig.interval)
     }
   }
 }
